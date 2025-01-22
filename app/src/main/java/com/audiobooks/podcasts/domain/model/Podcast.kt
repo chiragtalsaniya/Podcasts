20551357b0c577f624c1150eb7fed4c8666f40fc
Chirag Talsaniya <chirag.talsaniya20@gmail.com>
2025-01-22 17:53:32 -0500

    Add domain models and repository interfaces

---
+++ app/src/main/java/com/audiobooks/podcasts/domain/model/Podcast.kt
@@ -0,0 +1,32 @@
+package com.audiobooks.podcasts.domain.model
+
+
+data class Podcast(
+    val id: String,
+    val title: String,
+    val description: String,
+    val publisher: String,
+    val totalEpisodes: Int,
+    val thumbnail: String,
+    val image: String,
+    val rss: String?,
+    val website: String?,
+    val genreIds: List<Int>,
+    val language: String,
+    val country: String,
+    val explicitContent: Boolean,
+    val listennotesUrl: String,
+    val audioLengthSec: Int,
+    val latestPubDateMs: Long,
+    val earliestPubDateMs: Long,
+    val hasSponsors: Boolean,
+    val email: String?,
+    val latestEpisodeId: String,
+    val isClaimed: Boolean,
+    val listenScore: Int,
+    val listenScoreGlobalRank: String,
+    val hasGuestInterviews: Boolean,
+    val lookingFor: LookingFor,
+    val socialLinks: SocialLinks,
+    val isFavourite: Boolean = false // Default to false
+)

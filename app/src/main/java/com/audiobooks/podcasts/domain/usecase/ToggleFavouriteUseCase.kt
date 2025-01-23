a3d1a20e99c1d6949744c8518886d0dcf312479e
Chirag Talsaniya <chirag.talsaniya20@gmail.com>
2025-01-22 21:34:29 -0500

    Add use cases for fetching podcasts and toggling favorites

--- app/src/main/java/com/audiobooks/podcasts/domain/usecase/ToggleFavouriteUseCase.kt
+++ app/src/main/java/com/audiobooks/podcasts/domain/usecase/ToggleFavouriteUseCase.kt
@@ -2,13 +2,12 @@ package com.audiobooks.podcasts.domain.usecase
 
 import com.audiobooks.podcasts.domain.model.Podcast
 import com.audiobooks.podcasts.domain.repository.PodcastRepository
-import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class GetPodcastsUseCase @Inject constructor(
+class ToggleFavouriteUseCase @Inject constructor(
     private val repository: PodcastRepository
 ) {
-    suspend operator fun invoke(page: Int): Flow<List<Podcast>> {
-        return repository.getPodcasts(page)
+    suspend operator fun invoke(podcast: Podcast) {
+        repository.toggleFavourite(podcast)
     }
 }
